7d61c40c0d93af092b72961577d390b3a5990b7e
wangzhen <wangzhen24@baidu.com>
2017-10-26 12:19:15 +0000

    add make_vae_base model into ModelConstuctor.py to make the VAENMTModel

--- onmt/ModelConstructor.py
+++ onmt/ModelConstructor.py
@@ -8,7 +8,7 @@ import onmt
 import onmt.Models
 import onmt.modules
 from onmt.IO import ONMTDataset
-from onmt.Models import NMTModel, MeanEncoder, RNNEncoder, \
+from onmt.Models import VAENMTModel, NMTModel, MeanEncoder, RNNEncoder, \
                         StdRNNDecoder, InputFeedRNNDecoder
 from onmt.modules import Embeddings, ImageEncoder, CopyGenerator, \
                          TransformerEncoder, TransformerDecoder, \
@@ -173,6 +173,88 @@ def make_base_model(model_opt, fields, gpu, checkpoint=None):
 
     # add the generator to the module (does this register the parameter?)
     model.generator = generator
+
+    # Make the whole model leverage GPU if indicated to do so.
+    if gpu:
+        model.cuda()
+    else:
+        model.cpu()
+
+    return model
+
+def make_vae_base_model(model_opt, fields, gpu, checkpoint=None):
+    """
+    Args:
+        model_opt: the option loaded from checkpoint.
+        fields: `Field` objects for the model.
+        gpu(bool): whether to use gpu.
+        checkpoint: the model gnerated by train phase, or a resumed snapshot
+                    model from a stopped training.
+    Returns:
+        the NMTModel.
+    """
+    assert model_opt.model_type in ["text", "img"], \
+        ("Unsupported model type %s" % (model_opt.model_type))
+
+    # Make encoder.
+    assert model_opt.model_type == "text"
+
+    src_dict = fields["src"].vocab
+    feature_dicts = ONMTDataset.collect_feature_dicts(fields)
+    src_embeddings = make_embeddings(model_opt, src_dict,
+                                     feature_dicts)
+    c_encoder = make_encoder(model_opt, src_embeddings)
+    # Make decoder.
+    tgt_dict = fields["tgt"].vocab
+    # TODO: prepare for a future where tgt features are possible.
+    feature_dicts = []
+    tgt_embeddings = make_embeddings(model_opt, tgt_dict,
+                                     feature_dicts, for_encoder=False)
+    decoder = make_decoder(model_opt, tgt_embeddings)
+    u_encoder = make_encoder(model_opt, tgt_embeddings)
+
+    # Make NMTModel(= encoder + decoder).
+    model = VAENMTModel(model_opt, u_encoder, c_encoder, decoder)
+
+    # Make Generator.
+    assert not model_opt.copy_attn
+    generator = nn.Sequential(
+        nn.Linear(model_opt.rnn_size, len(fields["tgt"].vocab)),
+        nn.LogSoftmax())
+    if model_opt.share_decoder_embeddings:
+        generator[0].weight = decoder.embeddings.word_lut.weight
+
+    hidden_out_size = model_opt.dec_layers * model_opt.rnn_size
+    bow_project = nn.Sequential(
+        nn.Linear(model_opt.z_size + hidden_out_size, model_opt.bow_mid_size),
+        nn.Tanh(),
+        nn.Dropout(model_opt.dropout),
+        nn.Linear(model_opt.bow_mid_size, len(fields["tgt"].vocab)),
+        nn.LogSoftmax())
+    if model_opt.share_decoder_embeddings:
+        bow_project[3].weight = decoder.embeddings.word_lut.weight
+
+    # Load the model states from checkpoint or initialize them.
+    if checkpoint is not None:
+        print('Loading model parameters.')
+        model.load_state_dict(checkpoint['model'])
+        generator.load_state_dict(checkpoint['generator'])
+        bow_project.load_state_dict(checkpoint['bow_project'])
+    else:
+        if model_opt.param_init != 0.0:
+            print('Intializing parameters.')
+            for p in model.parameters():
+                p.data.uniform_(-model_opt.param_init, model_opt.param_init)
+        model.c_encoder.embeddings.load_pretrained_vectors(
+                model_opt.pre_word_vecs_enc, model_opt.fix_word_vecs_enc)
+        model.u_encoder.embeddings.load_pretrained_vectors(
+                model_opt.pre_word_vecs_enc, model_opt.fix_word_vecs_enc)
+        model.decoder.embeddings.load_pretrained_vectors(
+                model_opt.pre_word_vecs_dec, model_opt.fix_word_vecs_dec)
+
+    # add the generator to the module (does this register the parameter?)
+    model.generator = generator
+    model.bow_project = bow_project
 
     # Make the whole model leverage GPU if indicated to do so.
     if gpu:
