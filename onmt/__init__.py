f066fa45ae657878aa68b5605909915ce7976f46
wangzhen <wangzhen24@baidu.com>
2017-10-26 12:24:40 +0000

    add necessary including classes

--- onmt/__init__.py
+++ onmt/__init__.py
@@ -1,12 +1,12 @@
 import onmt.IO
 import onmt.Models
 import onmt.Loss
-from onmt.Trainer import Trainer, Statistics
-from onmt.Translator import Translator
+from onmt.Trainer import VAETrainer, Trainer, Statistics
+from onmt.Translator import Translator, VAETranslator
 from onmt.Optim import Optim
 from onmt.Beam import Beam, GNMTGlobalScorer
 
 
 # For flake8 compatibility
-__all__ = [onmt.Loss, onmt.IO, onmt.Models, Trainer, Translator,
+__all__ = [onmt.Loss, onmt.IO, onmt.Models, VAETrainer, Trainer, Translator, VAETranslator,
            Optim, Beam, Statistics, GNMTGlobalScorer]
