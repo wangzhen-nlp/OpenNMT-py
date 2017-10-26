ce75c7584d3ccd5356d2fa18323b0d3d7ebf2222
wangzhen <wangzhen24@baidu.com>
2017-10-26 12:22:37 +0000

    add VAETrainer into Trainer.py

--- onmt/Trainer.py
+++ onmt/Trainer.py
@@ -98,7 +98,7 @@ class Trainer(object):
         report_stats = Statistics()
 
         for i, batch in enumerate(self.train_iter):
-            target_size = batch.tgt.size(0)
+            target_size = batch.tgt[0].size(0)
             # Truncated BPTT
             trunc_size = self.trunc_size if self.trunc_size else target_size
 
@@ -157,7 +157,7 @@ class Trainer(object):
 
             # Compute loss.
             gen_state = onmt.Loss.make_gen_state(
-                outputs, batch, attns, (0, batch.tgt.size(0)))
+                outputs, batch, attns, (0, batch.tgt[0].size(0)))
             _, batch_stats = self.valid_loss(batch, **gen_state)
 
             # Update statistics.
@@ -188,6 +188,147 @@ class Trainer(object):
         checkpoint = {
             'model': model_state_dict,
             'generator': generator_state_dict,
+            'vocab': onmt.IO.ONMTDataset.save_vocab(fields),
+            'opt': opt,
+            'epoch': epoch,
+            'optim': self.optim
+        }
+        torch.save(checkpoint,
+                   '%s_acc_%.2f_ppl_%.2f_e%d.pt'
+                   % (opt.save_model, valid_stats.accuracy(),
+                      valid_stats.ppl(), epoch))
+
+class VAETrainer(object):
+    def __init__(self, model, train_iter, valid_iter,
+                 train_loss, valid_loss, optim,
+                 trunc_size, shard_size):
+        """
+        Args:
+            model: the seq2seq model.
+            train_iter: the train data iterator.
+            valid_iter: the validate data iterator.
+            train_loss: the train side LossCompute object for computing loss.
+            valid_loss: the valid side LossCompute object for computing loss.
+            optim: the optimizer responsible for lr update.
+            trunc_size: a batch is divided by several truncs of this size.
+            shard_size: compute loss in shards of this size for efficiency.
+        """
+        # Basic attributes.
+        self.model = model
+        self.train_iter = train_iter
+        self.valid_iter = valid_iter
+        self.train_loss = train_loss
+        self.valid_loss = valid_loss
+        self.optim = optim
+        self.trunc_size = trunc_size
+        self.shard_size = shard_size
+
+        # Set model in training mode.
+        self.model.train()
+
+    def train(self, epoch, report_func=None):
+        """ Called for each epoch to train. """
+        total_stats = Statistics()
+        report_stats = Statistics()
+
+        for i, batch in enumerate(self.train_iter):
+            target_size = batch.tgt[0].size(0)
+            # Truncated BPTT
+            trunc_size = self.trunc_size if self.trunc_size else target_size
+
+            dec_state = None
+            _, src_lengths = batch.src
+            _, tgt_lengths = batch.tgt
+
+            src = onmt.IO.make_features(batch, 'src')
+            tgt_outer = onmt.IO.make_features(batch, 'tgt')
+            report_stats.n_src_words += src_lengths.sum()
+
+            for j in range(0, target_size-1, trunc_size):
+                # 1. Create truncated target.
+                tgt = tgt_outer[j: j + trunc_size]
+
+                # 2. F-prop all but generator.
+                self.model.zero_grad()
+                outputs, attns, dec_state, kld, hidden = \
+                    self.model(src, tgt, src_lengths, tgt_lengths, dec_state)
+
+                # 3. Compute loss in shards for memory efficiency.
+                batch_stats = self.train_loss.sharded_compute_loss(
+                        batch, outputs, attns, j,
+                        trunc_size, self.shard_size, hidden, kld)
+
+                # 4. Update the parameters and statistics.
+                self.optim.step()
+                total_stats.update(batch_stats)
+                report_stats.update(batch_stats)
+
+                # If truncated, don't backprop fully.
+                if dec_state is not None:
+                    dec_state.detach()
+
+            if report_func is not None:
+                report_func(epoch, i, len(self.train_iter),
+                            total_stats.start_time, self.optim.lr,
+                            report_stats)
+                report_stats = Statistics()
+
+        return total_stats
+
+    def validate(self):
+        """ Called for each epoch to validate. """
+        # Set model in validating mode.
+        self.model.eval()
+
+        stats = Statistics()
+
+        for batch in self.valid_iter:
+            _, src_lengths = batch.src
+            _, tgt_lengths = batch.tgt
+            src = onmt.IO.make_features(batch, 'src')
+            tgt = onmt.IO.make_features(batch, 'tgt')
+
+            # F-prop through the model.
+            outputs, attns, _, kld, hidden = self.model(src, tgt, src_lengths, tgt_lengths)
+
+            # Compute loss.
+            gen_state = onmt.Loss.make_gen_state(
+                outputs, batch, attns, (0, batch.tgt[0].size(0)), hidden=hidden)
+            _, batch_stats = self.valid_loss(batch, **gen_state)
+
+            # Update statistics.
+            stats.update(batch_stats)
+
+        # Set model back to training mode.
+        self.model.train()
+
+        return stats
+
+    def epoch_step(self, ppl, epoch):
+        """ Called for each epoch to update learning rate. """
+        return self.optim.updateLearningRate(ppl, epoch)
+
+    def drop_checkpoint(self, opt, epoch, fields, valid_stats):
+        """ Called conditionally each epoch to save a snapshot. """
+        real_model = (self.model.module
+                      if isinstance(self.model, nn.DataParallel)
+                      else self.model)
+        real_generator = (real_model.generator.module
+                          if isinstance(real_model.generator, nn.DataParallel)
+                          else real_model.generator)
+        real_bow_project = (real_model.bow_project.module
+                            if isinstance(real_model.bow_project, nn.DataParallel)
+                            else real_model.bow_project)
+
+        model_state_dict = real_model.state_dict()
+        model_state_dict = {k: v for k, v in model_state_dict.items()
+                            if 'generator' not in k and 'bow_project' not in k}
+        generator_state_dict = real_generator.state_dict()
+        bow_project_state_dict = real_bow_project.state_dict()
+        checkpoint = {
+            'model': model_state_dict,
+            'generator': generator_state_dict,
+            'bow_project': bow_project_state_dict,
             'vocab': onmt.IO.ONMTDataset.save_vocab(fields),
             'opt': opt,
             'epoch': epoch,
